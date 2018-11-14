3fe0c07af67836d115a7f80d6330b0092ff25cd0
Vidar Nelson <vidar.nelson@gmail.com>
2018-11-14 14:48:46 +0100

    Added initial car sprites

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ tile_house = 1
 tile_road = 2
 
 num_players = 2
-player_radius = 1
+player_radius = 16
 
 input_keys = {
 	{
@@ -29,6 +29,7 @@ player_colors = {
 	{0,0.5,1,1},
 }
 
+
 function spring(x,target_x,v,k,d,dt)
 	local delta = target_x - x
 	local F = delta*k
@@ -37,12 +38,15 @@ function spring(x,target_x,v,k,d,dt)
 	return new_x, new_v
 end
 
+scale = 1.0
+
 function love.load(arg)
+	love.window.setMode(scale*1920,scale*1080,{resizable = false,vsync = true})
 	print("Load!")
 	for i_player =1,num_players do
 		local player = {}
-		player.x = 10 - 3*i_player
-		player.y = 5
+		player.x = 700 - 100*i_player
+		player.y = 500
 		player.vx = 0
 		player.vy = 0
 		player.angle = math.pi
@@ -50,23 +54,27 @@ function love.load(arg)
 		player.accel = 0
 		player.inputs = input_keys[i_player]
 		player.color = player_colors[i_player]
+		player.sprite = {
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0000.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0001.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0002.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0003.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0004.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0005.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0006.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0007.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0008.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0009.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0010.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0011.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0012.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0013.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0014.png"),
+			love.graphics.newImage("Assets/Cars/Skyline/Skyline_0015.png"),
+		}
 		players[i_player] = player
 	end
-
-	local map_image = love.image.newImageData("data/map.png")
-	map.w = map_image:getWidth()
-	map.h = map_image:getHeight()
-	map.tiles = {}
-	for y = 1,map.h do
-		for x = 1,map.w do 
-			local r, g, b, a = map_image:getPixel(x-1,y-1)
-			if r == 0 then
-				map.tiles[x+y*map.w] = tile_house
-			else
-				map.tiles[x+y*map.w] = tile_road
-			end
-		end
-	end
+	bkg_image = love.graphics.newImage("Assets/City/townmap_01.jpg")
 end
 
 
@@ -76,12 +84,17 @@ end
 function love.update(dt)
 	local pre_collision_players_pos = {}
 
+	
+
 	for i,player in pairs(players) do
 		local last_player
 		local target_speed = 0
 		local delta_angle = 0
 		if love.keyboard.isDown(player.inputs.up) then
-			target_speed = 16
+			target_speed = 200
+		end
+		if love.keyboard.isDown(player.inputs.down) then
+			target_speed = -50
 		end
 		if love.keyboard.isDown(player.inputs.left) then
 			delta_angle = -4*dt
@@ -89,8 +102,8 @@ function love.update(dt)
 		if love.keyboard.isDown(player.inputs.right) then
 			delta_angle =  4*dt
 		end
-		player.speed, player.accel =spring(player.speed, target_speed, player.accel, 80.0, 0.73, dt)
-		player.angle = player.angle + delta_angle
+		player.speed, player.accel =spring(player.speed, target_speed, player.accel, 80.0, 0.8, dt)
+		player.angle = player.angle + delta_angle * (player.speed)/200
 		local cos_angle = math.cos(player.angle)
 		local sin_angle = math.sin(player.angle)
 
@@ -98,7 +111,8 @@ function love.update(dt)
 		player.vy = player.vy * math.pow(0.001,dt)
 		local vm = math.sqrt(player.vx*player.vx + player.vy*player.vy)
 		local speed = player.speed
-		if vm > 1.0 then
+		if vm > 10 then
+			--player.angle = player.angle + 8*dt
 			speed = 0
 		else
 			--player.vx = 0
@@ -160,8 +174,8 @@ function love.update(dt)
 						t1 = t1 - 0.1
 						post_collision_players_pos[i].x = player.x + t1*dx2
 						post_collision_players_pos[i].y = player.y + t1*dy2
-						player.vx = player.vx + nx*1.7
-						player.vy = player.vy + ny*1.7
+						player.vx = player.vx + nx*17
+						player.vy = player.vy + ny*17
 						--print(nx .. " " .. ny .. "(" .. i .. ")")
 						--post_collision_players_pos[i].x = (1-t1)*player.x + t1*pre_pos_player.x
 						--post_collision_players_pos[i].y = (1-t1)*player.y + t1*pre_pos_player.y
@@ -183,27 +197,42 @@ function love.update(dt)
 	for i,player in pairs(players) do
 		player.x = post_collision_players_pos[i].x
 		player.y = post_collision_players_pos[i].y
+		while player.x < 0 do
+			player.x = player.x + 1920
+		end
+		while player.x > 1920 do
+			player.x = player.x - 1920
+		end
+		while player.y < 0 do
+			player.y = player.y + 1080
+		end
+		while player.y > 1080 do
+			player.y = player.y - 1080
+		end
 	end
 end
 
 function love.draw(dt)
 	love.graphics.push()
-	love.graphics.translate(10,10)
-	love.graphics.scale(10,10)
-	for y=1,map.w do
-		for x=1,map.w do
-			if map.tiles[x+y*map.w] == tile_house then
-				love.graphics.setColor(0.2,0.2,0.2,1.0)
-				love.graphics.rectangle("fill",x-0.25,y-0.25,0.5,0.5)
-			end
-		end
-	end
+	--love.graphics.translate(10,10)
+	love.graphics.scale(scale,scale)
+	love.graphics.setColor(1,1,1,1)
+	love.graphics.draw(bkg_image)
 
 	for i_player = 1,num_players do
 		local player = players[i_player]
 		love.graphics.setColor(player.color)
-		love.graphics.rectangle("fill",player.x-0.5,player.y-0.5,1,1)
-		love.graphics.circle("fill",player.x, player.y, player_radius)
+		--love.graphics.rectangle("fill",player.x-0.5,player.y-0.5,1,1)
+		--love.graphics.circle("fill",player.x, player.y, player_radius)
+		local sprite_index = math.mod(-player.angle*16/math.pi/2, 16)
+		sprite_index = math.floor(sprite_index+8.5)
+		while sprite_index < 0 do
+			sprite_index = sprite_index + 16
+		end
+		while sprite_index > 15 do
+			sprite_index = sprite_index - 16
+		end
+		love.graphics.draw(player.sprite[sprite_index+1],player.x-32,player.y-32)
 	end
 
 	love.graphics.pop()
