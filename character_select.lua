53e218b8fb73b779bf20688d1fa1b43e0831e16d
Vidar Nelson <vidar.nelson@gmail.com>
2018-11-15 17:08:36 +0100

    blaj

--- character_select.lua
+++ character_select.lua
@@ -1,44 +1,110 @@
-selections = {}
+local card_flip_anim = {}
 
 function load_character_select()
-    for i,player in pairs(players) do
-        selections[i] = player
-    end
+	for i,player in pairs(players) do
+		player.character_index = i
+	end
 end
 
 function update_character_select(dt)
+	for i,_ in pairs(card_flip_anim) do
+		card_flip_anim[i] = card_flip_anim[i] - dt*2
+	end
 end
 
 function love.keypressed(key)
-    if key == "escape" then
-        game_state = "game"
-        print("blajj")
-    end
+	if key == "escape" then
+		game_state = "game"
+		print("blajj")
+	end
+	for i,player in pairs(players) do 
+		if player.input_keys then 
+			for action,k in pairs(player.input_keys) do
+				if k == key then 
+					if player.active then 
+						if action == "left" then 
+							player.character_index = player.character_index - 1
+						end
+						if action == "right" then 
+							player.character_index = player.character_index + 1
+						end
+						if player.character_index < 1 then player.character_index = num_characters end
+						if player.character_index > num_characters then player.character_index = 1 end
+					else
+						card_flip_anim[i] = 1.0
+						player.active = true
+					end
+				end
+			end
+		end
+	end
 end
 
-function draw_character_select(dt)
-    local rect_w = 256
-    local rect_h = 256
-    local rect_spacing_x = 24
-    local rect_spacing_y = 16
-    local rows = 2
-    local cols = 4
-    local total_w = cols*rect_w + (cols-1)*rect_spacing_x
-    local total_h = rows*rect_h + (rows-1)*rect_spacing_y
-    local offset_y = 128
-    love.graphics.translate((1920-total_w)/2,(1080-total_h)/2-offset_y)
-    local i = 1
-    for r=1,rows do
-        for c=1,cols do
-            local player = selections[i]
-            if player then
-                love.graphics.setColor(player.color)
-            else
-                love.graphics.setColor(1,1,1,1)
-            end
-            love.graphics.rectangle("fill",(c-1)*(rect_w+rect_spacing_x), (r-1)*(rect_h+rect_spacing_y), rect_w, rect_h)
-            i = i + 1
-        end
-    end
+function love.gamepadpressed(gamepad, button)
+	for i,player in pairs(players) do 
+		if player.input_joystick and player.input_joystick == gamepad then 
+			if player.active then 
+				if button == "dpleft" then 
+					player.character_index = player.character_index - 1
+				end
+				if button == "dpright" then 
+					player.character_index = player.character_index + 1
+				end
+				if player.character_index < 1 then player.character_index = num_characters end
+				if player.character_index > num_characters then player.character_index = 1 end
+			else
+				card_flip_anim[i] = 1.0
+				player.active = true
+			end
+		end
+	end
+end
+
+function draw_character_select()
+	local rect_w = 400
+	local rect_h = 500
+	local rect_spacing_x = 80
+	local rect_spacing_y = 30
+	local rows = 2
+	local cols = 3
+	local total_w = cols*rect_w + (cols-1)*rect_spacing_x
+	local total_h = rows*rect_h + (rows-1)*rect_spacing_y
+	local offset_y = 0
+	love.graphics.translate((1920-total_w)/2,(1080-total_h)/2-offset_y)
+	local i = 1
+	for r=1,rows do
+		for c=1,cols do
+			local player = players[i]
+			local col = {0.2,0.2,0.2,1}
+			local character = nil
+			if player and player.active then
+				col = player.color
+				character = characters[player.character_index]
+			end
+			local x = (c-1)*(rect_w+rect_spacing_x)
+			local y = (r-1)*(rect_h+rect_spacing_y)
+
+			love.graphics.push()
+			if card_flip_anim[i] then
+				love.graphics.scale(card_flip_anim[i]*2-1,1)
+			end
+
+
+			love.graphics.setColor(col)
+			love.graphics.rectangle("fill",x, y, rect_w, rect_h)
+			love.graphics.setFont(main_font)
+			local text_col = {col[1]+0.4, col[2]+0.4, col[3]+0.4, 1}
+			love.graphics.setColor(text_col)
+			love.graphics.print("P"..i,x,y)
+
+			if character then
+				love.graphics.setColor(1,1,1,1)
+				love.graphics.print(character.name,x,y+rect_h-100)
+			end
+			love.graphics.pop()
+
+			i = i + 1
+		end
+	end
 end
 
--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ debug = true
 require("game")
 require("character_select")
 
-scale = 0.5
+scale = 1.0
 joysticks = {}
 num_players = 2
 players = {}
@@ -22,10 +22,16 @@ input_keys = {
 	}
 }
 player_colors = {
-	{1,0,0,1},
+	{1,0.5,0,1},
 	{0,0.5,1,1},
 	{0.5,1,0,1},
 }
+characters = {
+	{name="Greedo"},
+	{name="Dude"},
+	{name="Robot"},
+}
+num_characters = 3
 
 game_state = "character_select"
 
@@ -40,8 +46,10 @@ end
 
 function love.load(arg)
 	print("Load!")
-	love.window.setMode(scale*1920,scale*1080,{resizable = false,vsync = true})
 	num_keyboard_players = num_players
+	main_font = love.graphics.newFont("Assets/Fonts/Asap-SemiBold.ttf",30)
+	large_font = love.graphics.newFont("Assets/Fonts/Asap-SemiBold.ttf",80)
+	love.graphics.setFont(main_font)
 	joysticks = love.joystick.getJoysticks()
 	print("Joysticks:")
 	for _,joystick in pairs(joysticks) do 
@@ -49,7 +57,7 @@ function love.load(arg)
 		num_players = num_players +1
 	end
 	for i_player =1,num_players do
-		local player = {}
+		local player = {active=false}
 		if i_player <= num_keyboard_players then
 			player.input_keys = input_keys[i_player]
 		else
@@ -57,28 +65,28 @@ function love.load(arg)
 		end
 		player.color = player_colors[i_player]
 		players[i_player] = player
-    end
-    load_game()
-    load_character_select()
+	end
+	load_game()
+	load_character_select()
 end
 
 function love.update(dt)
-    if game_state == "game" then
-        update_game(dt)
-    end
-    if game_state == "character_select" then
-        update_character_select(dt)
-    end
+	if game_state == "game" then
+		update_game(dt)
+	end
+	if game_state == "character_select" then
+		update_character_select(dt)
+	end
 end
 
-function love.draw(dt)
+function love.draw()
 	love.graphics.push()
 	love.graphics.scale(scale,scale)
-    if game_state == "game" then
-        draw_game(dt)
-    end
-    if game_state == "character_select" then
-        draw_character_select(dt)
-    end
+	if game_state == "game" then
+		draw_game()
+	end
+	if game_state == "character_select" then
+		draw_character_select()
+	end
 	love.graphics.pop()
 end
